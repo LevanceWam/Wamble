6436e489de8007c61560890175fa0ae8e9ae556d
LevanceWam <lkwamley@fullsail.edu>
2016-08-20 21:37:23 -0400

    I believe I found my problem it might be the fact that on codeship it was looking for the log file that isnt here on the debugger because it is elsewhere

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 exports.debug = (title, obj) => {
-  const fs = require('fs');
+  // const fs = require('fs');
 
   const ts = new Date();
   const seperator = '\n------------------------------\n';
@@ -7,11 +7,9 @@ exports.debug = (title, obj) => {
 
   const outputTERM = seperator.lightCyan + title.magenta.underline + ' ' + obj + '\n' + seperator.lightCyan + ' ' + ts;
 
-  const outputFILE = seperator + title + ' ' + obj + '\n' + seperator + ' ' + ts;
+  // const outputFILE = seperator + title + ' ' + obj + '\n' + seperator + ' ' + ts;
+
   if (process.env.DEBUG) {
-    fs.appendFile('lib/logs/util.log', outputFILE, (err) => {
-      if (err) throw err;
-      console.log(outputTERM);
-    });
+    console.log(outputTERM);
   }
 };
