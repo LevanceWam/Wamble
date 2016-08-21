888916d0e488fbc3955b20751577d77365021dfb
LevanceWam <lkwamley@fullsail.edu>
2016-08-20 22:45:13 -0400

    I want to see something

--- test/__util.js
+++ test/__util.js
@@ -1,6 +1,10 @@
 const util = require('../src/util');
 const expect = require('chai').expect;
 
+beforeEach(() => {
+  util.debug('Before each of');
+});
+
 describe('Wamble Gamble', () => {
   it('I should pass, This should read this as a string', (done) => {
     expect(util.output).to.not.equal(null);
