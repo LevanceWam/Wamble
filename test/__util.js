152a48d858e6106f13fd976f51623f3c0340c5b5
LevanceWam <lkwamley@fullsail.edu>
2016-08-20 21:25:03 -0400

    Just change the describe message to wamble gamble up the patch version looking for whats wrong with the codeship throwing an error about an unexpected )

--- test/__util.js
+++ test/__util.js
@@ -1,8 +1,7 @@
 const util = require('../src/util');
 const expect = require('chai').expect;
 
-
-describe('Ramble Wamble ', () => {
+describe('Wamble Gamble', () => {
   it('I should pass, This should read this as a string', (done) => {
     expect(util.output).to.not.equal(null);
     done();
