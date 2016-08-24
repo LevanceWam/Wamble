12552a293c8836a04719d812e66a468bceba3393
KJGilkes <khalilgilkes@Khalils-MacBook-Pro.local>
2016-08-24 18:59:32 -0400

    Fixed functionality of debug tool

--- src/util.js
+++ src/util.js
@@ -1,15 +1,21 @@
+const ts = new Date();
+const seperator = '\n------------------------------\n';
+const fs = require('fs');
+require('terminal-colors');
+
 exports.debug = (title, obj) => {
-  // const fs = require('fs');
-
-  const ts = new Date();
-  const seperator = '\n------------------------------\n';
-  require('terminal-colors');
-
-  const outputTERM = seperator.lightCyan + title.magenta.underline + ' ' + obj + '\n' + seperator.lightCyan + ' ' + ts;
-
+  const goodoutputTERM = seperator.lightCyan + title.magenta.underline + ' ' + obj + '\n' + seperator.lightCyan + ' ' + ts;
+  const outputTERM = seperator.lightCyan + title.magenta.underline + seperator.lightCyan + ' ' + ts;
   // const outputFILE = seperator + title + ' ' + obj + '\n' + seperator + ' ' + ts;
-
   if (process.env.DEBUG) {
-    console.log(outputTERM);
+    if (obj === undefined) {
+      console.log(outputTERM);
+      fs.appendFile('logs/logfile.log', title, () => {
+      });
+    } else {
+      console.log(goodoutputTERM);
+      fs.appendFile('logs/logfile.log', title, obj, () => {
+      });
+    }
   }
 };
