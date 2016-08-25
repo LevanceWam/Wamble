5b9763add55c6e9d4f225831101ba0b6caaeb88e
KJGilkes <khalilgilkes@Khalils-MacBook-Pro.local>
2016-08-25 04:13:15 -0400

    Added version bumping tests

--- test/__util.js
+++ test/__util.js
@@ -25,3 +25,26 @@ describe('Wamble Gamble', () => {
     done();
   });
 });
+
+describe('Version Bumper', () => {
+  const number = '2.0.0';
+  let realNum;
+
+  it('Should be able to increment the major number', (done) => {
+    realNum = util.versBumper(number, 'major');
+    expect(realNum).to.be.equal('3.0.0');
+    done();
+  });
+
+  it('Should be able to increment the minor number', (done) => {
+    realNum = util.versBumper(number, 'minor');
+    expect(realNum).to.be.equal('2.1.0');
+    done();
+  });
+
+  it('Should be able to increment the patch number', (done) => {
+    realNum = util.versBumper(number, 'patch');
+    expect(realNum).to.be.equal('2.0.1');
+    done();
+  });
+});
